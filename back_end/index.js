b3796b892a242ee915a4ad0b1c5bb04c284a1640
Meronmeron <Mmer3725@gmail.com>
2024-09-07 13:31:48 +0300

    updated

--- back_end/index.js
+++ back_end/index.js
@@ -82,18 +82,26 @@ app.use(cookieParser());
 
 
 app.get("/oauth", (req, res) => {
-    const csrfState = Math.random().toString(36).substring(2);
-    res.cookie("csrfState", csrfState, { maxAge: 60000 });
+  try {
+      const csrfState = Math.random().toString(36).substring(2);
+      res.cookie("csrfState", csrfState, { maxAge: 60000 });
+
       let url = "https://www.tiktok.com/v2/auth/authorize/";
-      // the following params need to be in `application/x-www-form-urlencoded` format.
       url += "?client_key=aw0h2vs3s39ad7dk";
       url += "&scope=user.info.basic,video.upload,video.publish";
       url += "&response_type=code";
-      url +=
-      "&redirect_uri=https://redirect-uri-tan.vercel.app/redirect";
+      url += "&redirect_uri=https://redirect-uri-tan.vercel.app/redirect";
       url += "&state=" + csrfState;
-    res.json({ url: url });
-  });
+
+      console.log(`[OAuth Request] URL generated: ${url}`);
+      
+      res.json({ url: url });
+  } catch (error) {
+      console.error(`[OAuth Error] An error occurred: ${error.message}`);
+      res.status(500).json({ error: "Internal server error" });
+  }
+});
+
 
   app.post("/tiktokaccesstoken", async (req, res) => {
     try {
